6930e9abbc9f291e2398de39dac6800d68cf8919
Aubrey <aubreyfinkelstein@gmail.com>
2023-03-24 20:34:55 -0700

    added minDepth DFS version and notes from doing problem with Kevin

--- assorted-leetcode/min-depth-binary-tree.js
+++ assorted-leetcode/min-depth-binary-tree.js
@@ -26,7 +26,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function(root) {
+var minDepthBFS = function(root) {
     // since we want to find at top of tree, breadth first will get us there soonest
     // if root is null then depth is 0
     // put node & level its at in queue (array w/push & shift) & do breadth first traversal
@@ -36,8 +36,31 @@ var minDepth = function(root) {
     let queue = [[root, 1]];
     while(queue.length > 0){
         const [node, level] = queue.shift();
-        if(node.left === null & node.right === null) return level;
+        if(node.left === null && node.right === null) return level;
         if(node.left !== null) queue.push([node.left, level + 1]);
         if(node.right !== null) queue.push([node.right, level + 1]);
     }
 };
+
+
+// per kevin group, queue version is less memory in smaller graphs/trees but can go way over memory bounds in large graphs
+// depth first will prevent that, but recursive can get stack overflow, so in very large cases DFS iterative will actually be best
+// he also mentioned there's a DFS version that limits how far it looks at a time for big graphs/trees
+// LD DFS - limited depth DFS -> do DFS but only to given depth
+// ID DFS - iterative deepening DFS -> for D=1 to limit do LD DFS on root with D
+function minDepth(root){
+    if(root === null) return 0;
+
+    let minLevel;
+    let stack = [[root, 1]];
+    while(stack.length > 0){
+        const [node, level] = stack.pop();
+        if(node.left === null && node.right === null){
+            if(level < minLevel || minLevel === undefined) minLevel = level;
+        }
+        if(node.left !== null) stack.push([node.left, level + 1]);
+        if(node.right !== null) stack.push([node.right, level + 1]);
+    }
+
+    return minLevel;
+}
